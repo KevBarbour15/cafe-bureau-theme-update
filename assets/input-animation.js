f214cf65b306ee38cb149895a1f80b19d6c427a5
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-12-09 15:30:32 -0500

    updated footer

---
+++ assets/input-animation.js
@@ -0,0 +1,36 @@
+class InputAnimation extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.input = document.getElementsByClassName('subscribe-input');
+
+
+    console.log(this.input);
+
+    if (!this.input) return;
+
+    this.inputAnimation = gsap.timeline({ repeat: -1 })
+      .to(this.input, {
+        opacity: 0.3,
+        duration: 0.65,
+        ease: "power1.out"
+      }, 0.25)
+      .to(this.input, {
+        opacity: 1,
+        duration: 0.65,
+        ease: "power1.in"
+      }, 0.95)
+    
+    
+
+    this.input.addEventListener('focus', () => this.inputAnimation);
+    this.input.addEventListener('blur', () => this.inputAnimation.reverse());
+  }
+
+
+}
+
+customElements.define('input-animation', InputAnimation);
+
