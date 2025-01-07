042f76ff61a4b30bd065f13b568dd424bbe80e13
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2025-01-07 17:43:26 -0500

    added cafe updates

---
+++ assets/gate-animation.js
@@ -0,0 +1,23 @@
+class GateAnimation  extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.gateOverlay = this.querySelector('#gate-overlay');
+
+    this.gateAnimation = gsap.to(this.gateOverlay, {
+      //paused: true,
+      y: '-100vh',
+     ease: "sine.inOut",
+      delay: 1, 
+      duration: 1.25,
+
+      onComplete: () => {
+        this.gateOverlay.style.display = 'none';
+      },
+    });
+  }
+}
+
+customElements.define('gate-animation', GateAnimation);
