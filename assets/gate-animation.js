97632c119820ac54c3b3dbdaf1319cc5df4adcff
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2025-02-18 13:41:25 -0500

    added screen scroll freeze until gate animation is complete

--- assets/gate-animation.js
+++ assets/gate-animation.js
@@ -1,20 +1,32 @@
-class GateAnimation  extends HTMLElement {
+class GateAnimation extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
     this.gateOverlay = this.querySelector('#gate-overlay');
+    this.html = document.querySelector('html');
+    this.window = window;
+    
+    
+    if (window.scrollY !== 0) {
+      this.window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+    this.html.style.overflowY = 'hidden';
 
     this.gateAnimation = gsap.to(this.gateOverlay, {
-      //paused: true,
       y: '-100vh',
-     ease: "sine.inOut",
-      delay: 1.25, 
-      duration: 1.25,
+      ease: "linear",
+      delay: 1,
+      duration: 1.15,
 
       onComplete: () => {
         this.gateOverlay.style.display = 'none';
+        this.html.style.overflowY = 'auto';
       },
     });
   }
